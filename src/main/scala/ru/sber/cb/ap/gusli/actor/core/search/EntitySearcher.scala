782a4e4785c9704f64bde3fc777d04bf97c0d937
Kostya Kviz <kviz@outlook.com>
2018-09-14 20:32:27 +0300

    implement simple EntitySearcher logic

--- src/main/scala/ru/sber/cb/ap/gusli/actor/core/search/EntitySearcher.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/core/search/EntitySearcher.scala
@@ -1,6 +1,8 @@
 package ru.sber.cb.ap.gusli.actor.core.search
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import ru.sber.cb.ap.gusli.actor.core.Entity.{ChildrenEntityList, EntityMetaResponse, GetChildren, GetEntityMeta}
+import ru.sber.cb.ap.gusli.actor.core.Project.EntityFound
 
 object EntitySearcher {
   def apply(entityRefs: Seq[ActorRef], entityId: Long, replyTo: ActorRef): Props = Props(new EntitySearcher(entityRefs, entityId, replyTo))
@@ -9,12 +11,17 @@ object EntitySearcher {
 class EntitySearcher(entityRefs: Seq[ActorRef], entityId: Long, replyTo: ActorRef) extends Actor with ActorLogging {
   override def preStart(): Unit = {
     log.info("EntitySearcher entityId={} replyTo={} entities={}", entityId, replyTo, entityRefs)
-
+    entityRefs.foreach(_ ! GetEntityMeta())
   }
 
   override def receive: Receive = {
-    case msg =>
-      replyTo ! msg
-      context.stop(self)
+    case m@EntityMetaResponse(id, _, _) =>
+      val entity = sender()
+      if (id == entityId)
+        replyTo ! EntityFound(m, entity)
+      else
+        entity ! GetChildren()
+    case ChildrenEntityList(kids)=>
+      context.actorOf(EntitySearcher(kids, entityId, self))
   }
 }
--- src/test/scala/ru/sber/cb/ap/gusli/actor/core/search/EntitySearcherSpec.scala
+++ src/test/scala/ru/sber/cb/ap/gusli/actor/core/search/EntitySearcherSpec.scala
@@ -3,25 +3,31 @@ package ru.sber.cb.ap.gusli.actor.core.search
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import ru.sber.cb.ap.gusli.actor.core.Entity.{EntityMetaResponse, GetEntityMeta}
 import ru.sber.cb.ap.gusli.actor.core.EntityMetaDefault
 import ru.sber.cb.ap.gusli.actor.core.Project.EntityFound
 
 
 class EntitySearcherSpec extends TestKit(ActorSystem("EntitySearcherSpec")) with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
-  val entity1 = TestProbe()
-  val entity2 = TestProbe()
-  val metaEntity1 = EntityMetaDefault(1, "entity1", "/entity1")
-  val metaEntity2 = EntityMetaDefault(2, "entity2", "/entity2")
-  val entityRequester = TestProbe()
-
   override def afterAll: Unit = {
     TestKit.shutdownActorSystem(system)
   }
 
-  "A EntitySearcher" must {
+  "EntitySearcher" must {
+    val entity1 = TestProbe()
+    val entity2 = TestProbe()
+    val meta1 = EntityMetaDefault(1, "entity1", "/entity1")
+    val meta2 = EntityMetaDefault(2, "entity2", "/entity2")
+    val entityRequester = TestProbe()
+
     "send back EntityFound" in {
       system.actorOf(EntitySearcher(entityRefs = Seq(entity1.ref, entity2.ref), 2, entityRequester.ref))
-      entityRequester.expectMsg(EntityFound(metaEntity2, entity2.ref))
+      entity1.expectMsg(GetEntityMeta())
+      entity1.reply(EntityMetaResponse(meta1.id, meta1.name, meta1.path))
+      entity2.expectMsg(GetEntityMeta())
+      entity2.reply(EntityMetaResponse(meta2.id, meta2.name, meta2.path))
+
+      entityRequester.expectMsg(EntityFound(EntityMetaResponse(meta2.id, meta2.name, meta2.path), entity2.ref))
     }
   }
 }
