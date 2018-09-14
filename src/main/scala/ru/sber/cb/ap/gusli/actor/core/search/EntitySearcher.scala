70b3a1347b343447c6956ea014b12a0fd9606585
Kostya Kviz <kviz@outlook.com>
2018-09-14 21:29:23 +0300

    implement EntitySearcher

--- src/main/scala/ru/sber/cb/ap/gusli/actor/core/search/EntitySearcher.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/core/search/EntitySearcher.scala
@@ -2,26 +2,43 @@ package ru.sber.cb.ap.gusli.actor.core.search
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import ru.sber.cb.ap.gusli.actor.core.Entity.{ChildrenEntityList, EntityMetaResponse, GetChildren, GetEntityMeta}
-import ru.sber.cb.ap.gusli.actor.core.Project.EntityFound
+import ru.sber.cb.ap.gusli.actor.core.Project.{EntityFound, EntityNotFound}
 
 object EntitySearcher {
   def apply(entityRefs: Seq[ActorRef], entityId: Long, replyTo: ActorRef): Props = Props(new EntitySearcher(entityRefs, entityId, replyTo))
 }
 
 class EntitySearcher(entityRefs: Seq[ActorRef], entityId: Long, replyTo: ActorRef) extends Actor with ActorLogging {
+  val entityRefsSize = entityRefs.size * 2
+  var count = 0
+
   override def preStart(): Unit = {
     log.info("EntitySearcher entityId={} replyTo={} entities={}", entityId, replyTo, entityRefs)
-    entityRefs.foreach(_ ! GetEntityMeta())
+    if (entityRefs.isEmpty)
+      replyTo ! EntityNotFound(entityId)
+    else
+      entityRefs.foreach(_ ! GetEntityMeta())
   }
 
   override def receive: Receive = {
     case m@EntityMetaResponse(id, _, _) =>
+      checkNotFound()
       val entity = sender()
       if (id == entityId)
         replyTo ! EntityFound(m, entity)
       else
         entity ! GetChildren()
-    case ChildrenEntityList(kids)=>
+    case ChildrenEntityList(kids) =>
+      checkNotFound()
       context.actorOf(EntitySearcher(kids, entityId, self))
+  }
+
+  def checkNotFound(): Unit = {
+    count = count + 1
+    log.info("count={}", count)
+    if (entityRefsSize == count) {
+      replyTo ! EntityNotFound(entityId)
+      context.stop(self)
+    }
   }
 }
