88d4ee76c1f46da88fffcec3eb05605e13d9db07
Kostya Kviz <kviz@outlook.com>
2018-09-17 12:06:00 +0300

    add entityRoot, categoryRoot

--- src/main/scala/ru/sber/cb/ap/gusli/actor/core/Project.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/core/Project.scala
@@ -7,17 +7,19 @@ object Project {
   def apply(meta: ProjectMeta): Props = {
     Props(new Project(meta))
   }
-  
+
   case class GetProjectMeta(replyTo: Option[ActorRef] = None) extends Request
 
   case class GetCategoryRoot(replyTo: Option[ActorRef] = None) extends Request
 
   //entity requests
   case class GetEntityRoot(replyTo: Option[ActorRef] = None) extends Request
+
   case class FindEntity(entityId: Long, replyTo: Option[ActorRef] = None) extends Request
 
   //entity responses
   case class EntityFound(meta: EntityMeta, entityRef: ActorRef) extends Response
+
   case class EntityNotFound(entityId: Long) extends Response
 
 
@@ -30,10 +32,13 @@ object Project {
 
 class Project(meta: ProjectMeta) extends BaseActor {
   import Project._
+  val entityRoot = context.actorOf(Entity(EntityMetaDefault(0, "entity", "")), "entity")
+  val categoryRoot = context.actorOf(Category(CategoryMetaDefault("category"), context.self), "category")
+
   override def receive: Receive = {
     case GetProjectMeta(sendTo) => sendTo getOrElse sender ! ProjectMetaResponse(meta.name)
-    case GetCategoryRoot(sendTo) => sendTo getOrElse sender ! CategoryRoot(context.self)
-    case GetEntityRoot(sendTo) => sendTo getOrElse sender ! EntityRoot(context.self)
+    case GetCategoryRoot(sendTo) => sendTo getOrElse sender ! CategoryRoot(categoryRoot)
+    case GetEntityRoot(sendTo) => sendTo getOrElse sender ! EntityRoot(entityRoot)
     case FindEntity(id, sendTo) => sendTo getOrElse sender ! EntityNotFound(id)
   }
 }
@@ -42,4 +47,4 @@ trait ProjectMeta {
   def name: String
 }
 
-case class ProjectMetaDefault(name:String) extends ProjectMeta
+case class ProjectMetaDefault(name: String) extends ProjectMeta
