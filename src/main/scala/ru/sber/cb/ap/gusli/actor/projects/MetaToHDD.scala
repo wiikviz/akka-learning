89bf2803547b741c820e319ac82c70720dc26864
aslanbek <men123123men@yandex.ru>
2018-09-18 20:12:33 +0300

    entity writer realized

--- src/main/scala/ru/sber/cb/ap/gusli/actor/projects/MetaToHDD.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/projects/MetaToHDD.scala
@@ -1,26 +1,19 @@
 package ru.sber.cb.ap.gusli.actor.projects
 
 import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Path}
 
 import ru.sber.cb.ap.gusli.actor.core.{CategoryMeta, EntityMeta, ProjectMeta, WorkflowMeta}
+import ru.sber.cb.ap.gusli.actor.projects.MetaToYamlSerialization._
 
 object MetaToHDD {
+  val charSet = StandardCharsets.UTF_8
 
   def writeProjectMetaToPath(meta: ProjectMeta, path: Path):Path =
     createNewFolder(meta.name, path)
 
-  //  trait CategoryMeta {
-  //    def name: String
-  //    def sqlMap: Map[String, String]
-  //    def init: Map[String, String]
-  //    def user: Option[String]
-  //    def queue: Option[String]
-  //    def grenkiVersion: Option[String]
-  //    def params: Map[String, String]
-  //    def stats: Set[Long]
-  //    def entities: Set[Long]
-  //  }
+
   def writeCategoryMetaToPath(meta:CategoryMeta, dir:Path, parentMeta:CategoryMeta): Path ={
     val categoryFolder =  createNewFolder(meta.name, dir)
 
@@ -32,24 +25,8 @@ object MetaToHDD {
 
 
 
-
-  //  trait WorkflowMeta {
-  //    def name: String
-  //    def sql: Map[String, String]
-  //    def sqlMap: Map[String, String]
-  //    def init: Map[String, String]
-  //    def user: Option[String]
-  //    def queue: Option[String]
-  //    def grenkiVersion: Option[String]
-  //    def params: Map[String, String]
-  //    def stats: Set[Long]
-  //    def entities: Set[Long]
-  //  }
   def writeWorkflowMetaToPath(meta:WorkflowMeta, dir:Path, categoryMeta: CategoryMeta): Path ={
     val workflowFolder = createNewFolder(meta.name, dir)
-
-
-
 
 
 
@@ -60,30 +37,28 @@ object MetaToHDD {
 
 
 
-  //trait EntityMeta {
-  //  def id: Long
-  //  def name: String
-  //  def path: String
-  //  def parentId: Option[Long]
-  //  def storage = "HDFS"
-  //}
+
   def writeEntityMetaToPath(meta: EntityMeta, dir:Path, parentMeta:EntityMeta, hasChildren:Boolean): Path ={
-    val entityFolder = createNewFolder(meta.name, dir)
+    val fileContent = convertEntityMetaToYAMLFileContent(meta, parentMeta)
+    if(hasChildren){
+      val entityFolder = createNewFolder(meta.name, dir)
+      writeYAMLTextFileToDirectory(fileContent,"entity", entityFolder)
+      entityFolder
+    }
+    else
+      writeYAMLTextFileToDirectory(fileContent,s"${meta.id} ${meta.id}",dir)
 
-
-
-
-
-
-    entityFolder
   }
 
-  private def createNewFolder(newFolderName:String, dirWereWillBeCreatedNewFilder:Path): Path = {
-    Files createDirectories dirWereWillBeCreatedNewFilder resolve normalizeName(newFolderName)
+  private def createNewFolder(newFolderName:String, dirWereWillBeCreatedNewFolder:Path): Path = {
+    Files.createDirectories(dirWereWillBeCreatedNewFolder resolve normalizeName(newFolderName))
   }
 
   private def normalizeName(folderName: String):String = {
     val tt = folderName.replace("/","~").replace(":","%3A").trim
-    URLEncoder.encode(folderName, "UTF-8")
+    URLEncoder.encode(folderName, charSet)
+  }
+  def writeYAMLTextFileToDirectory(fileContent:String, fileName:String, dir:Path):Path ={
+    Files.write(dir.resolve(fileName+".yaml"),fileContent.getBytes(charSet))
   }
 }
--- src/main/scala/ru/sber/cb/ap/gusli/actor/projects/MetaToYamlSerialization.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/projects/MetaToYamlSerialization.scala
@@ -3,25 +3,57 @@ package ru.sber.cb.ap.gusli.actor.projects
 import ru.sber.cb.ap.gusli.actor.core.{CategoryMeta, EntityMeta, WorkflowMeta}
 
 object MetaToYamlSerialization {
+  //  trait CategoryMeta {
+  //    def name: String
+  //    def sqlMap: Map[String, String]
+  //    def init: Map[String, String]
+  //    def user: Option[String]
+  //    def queue: Option[String]
+  //    def grenkiVersion: Option[String]
+  //    def params: Map[String, String]
+  //    def stats: Set[Long]
+  //    def entities: Set[Long]
+  //  }
 
-  def convertCategoryMetaToYAMLFileContent(meta:CategoryMeta, parentMeta:CategoryMeta): String ={
 
+  //  trait WorkflowMeta {
+  //    def name: String
+  //    def sql: Map[String, String]
+  //    def sqlMap: Map[String, String]
+  //    def init: Map[String, String]
+  //    def user: Option[String]
+  //    def queue: Option[String]
+  //    def grenkiVersion: Option[String]
+  //    def params: Map[String, String]
+  //    def stats: Set[Long]
+  //    def entities: Set[Long]
+  //  }
+
+  //trait EntityMeta {
+  //  def id: Long
+  //  def name: String
+  //  def path: String
+  //  def parentId: Option[Long]
+  //  def storage = "HDFS"
+  //}
+  def convertCategoryMetaToYAMLFileContent(meta: CategoryMeta, parentMeta: CategoryMeta): String = {
 
 
     "CategoryMeta  YAMLfile  Content"
   }
 
-  def convertWorkflowMetaToYAMLFileContent(meta:WorkflowMeta, categoryMeta: CategoryMeta): String ={
-
+  def convertWorkflowMetaToYAMLFileContent(meta: WorkflowMeta, categoryMeta: CategoryMeta): String = {
 
 
     "WorkflowMeta  YAMLfile  Content"
   }
 
-  def convertEntityMetaToYAMLFileContent(meta: EntityMeta, parentMeta:EntityMeta): String ={
-
-
-
-    "EntityMeta  YAMLfile  Content"
+  def convertEntityMetaToYAMLFileContent(meta: EntityMeta, parentMeta: EntityMeta): String = {
+    s"""---
+       |name: "${meta.name}"
+       |path: "${meta.path}"
+       |id: ${meta.id}
+       |storage: "${meta.storage}"
+       |parentId: ${meta.parentId}""".stripMargin
   }
 }
