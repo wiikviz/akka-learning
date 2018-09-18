195c3726e66d832336c8edd10195ad1957d66818
Kviz <kviz@outlook.com>
2018-09-18 22:35:55 +0300

    WorkflowDtoExtractor WIP #4

--- src/main/scala/ru/sber/cb/ap/gusli/actor/core/diff/dto/WorkflowDtoExtractor.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/core/diff/dto/WorkflowDtoExtractor.scala
@@ -1,7 +1,9 @@
 package ru.sber.cb.ap.gusli.actor.core.diff.dto
 
 import akka.actor.{ActorRef, Props}
-import ru.sber.cb.ap.gusli.actor.core.Workflow.{GetWorkflowMeta, WorkflowMetaResponse}
+import ru.sber.cb.ap.gusli.actor.core.Entity.GetEntityMeta
+import ru.sber.cb.ap.gusli.actor.core.Workflow.{EntityList, GetEntityList, GetWorkflowMeta, WorkflowMetaResponse}
+import ru.sber.cb.ap.gusli.actor.core.{EntityMeta, WorkflowMeta}
 import ru.sber.cb.ap.gusli.actor.{BaseActor, Request, Response}
 
 object WorkflowDtoExtractor {
@@ -16,22 +18,30 @@ object WorkflowDtoExtractor {
 class WorkflowDtoExtractor extends BaseActor {
   type Workflow = ActorRef
   type Requester = ActorRef
+  type Entity = ActorRef
 
   import WorkflowDtoExtractor._
 
   var awaitExtract: Map[Workflow, Requester] = Map.empty
+  var wfMeta: Map[Workflow, WorkflowMeta] = Map.empty
+  var entityIds: Map[Workflow, Seq[Long]] = Map.empty
+  var entityMetas: Map[Long, EntityMeta] = Map.empty
 
   override def receive: Receive = {
     case Extract(wf, sendTo) =>
       val replyTo = sendTo getOrElse sender
       awaitExtract = awaitExtract + (wf -> replyTo)
       wf ! GetWorkflowMeta()
+      wf ! GetEntityList()
     case WorkflowMetaResponse(m) =>
       //todo: replace for unapply
       val dto = WorkflowDto(m.name, m.sql, m.sqlMap, m.init, m.user, m.queue, m.grenkiVersion, m.params, m.stats, Set.empty)
 
-      val wf = sender()
-      awaitExtract(wf) ! WorkflowExtracted(dto)
-      awaitExtract = awaitExtract - wf
+      val wf: Workflow = sender()
+      wfMeta = wfMeta + (wf -> m)
+      //awaitExtract(wf) ! WorkflowExtracted(dto)
+      //awaitExtract = awaitExtract - wf
+    case EntityList(entities)=>
+      entities.foreach(_ ! GetEntityMeta())
   }
 }
