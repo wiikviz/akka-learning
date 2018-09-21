bf9aa84493aaafc8001f4db696509ec5f8a4bdb5
Kviz <kviz@outlook.com>
2018-09-21 12:45:57 +0300

    add WorkflowComparerForEqualsSpec

--- src/main/scala/ru/sber/cb/ap/gusli/actor/core/comparer/WorkflowComparer.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/core/comparer/WorkflowComparer.scala
@@ -1,7 +1,9 @@
 package ru.sber.cb.ap.gusli.actor.core.comparer
 
-import akka.actor.Actor.emptyBehavior
 import akka.actor.{ActorRef, Props}
+import ru.sber.cb.ap.gusli.actor.core.dto.WorkflowDto
+import ru.sber.cb.ap.gusli.actor.core.extractor.WorkflowDtoExtractor
+import ru.sber.cb.ap.gusli.actor.core.extractor.WorkflowDtoExtractor.WorkflowExtracted
 import ru.sber.cb.ap.gusli.actor.{BaseActor, Response}
 
 object WorkflowComparer {
@@ -14,5 +16,35 @@ object WorkflowComparer {
 }
 
 class WorkflowComparer(wf1: ActorRef, wf2: ActorRef, receiver: ActorRef) extends BaseActor {
-  override def receive: Receive = emptyBehavior
+
+  import WorkflowComparer._
+
+  var dto1: Option[WorkflowDto] = None
+  var dto2: Option[WorkflowDto] = None
+
+  var ext1: ActorRef = _
+  var ext2: ActorRef = _
+
+  override def preStart(): Unit = {
+    ext1 = context.actorOf(WorkflowDtoExtractor(wf1, self))
+    ext2 = context.actorOf(WorkflowDtoExtractor(wf2, self))
+  }
+
+
+  override def receive: Receive = {
+    case WorkflowExtracted(dto) =>
+      if (sender() == ext1)
+        dto1 = Some(dto)
+      else if (sender() == ext2)
+        dto2 = Some(dto)
+
+      for (d1 <- dto1; d2 <- dto2) {
+        if (d1 == d2)
+          receiver ! WorkflowEquals(wf1, wf2)
+        else
+          receiver ! WorkflowNotEquals(wf1, wf2)
+
+        context.stop(self)
+      }
+  }
 }
