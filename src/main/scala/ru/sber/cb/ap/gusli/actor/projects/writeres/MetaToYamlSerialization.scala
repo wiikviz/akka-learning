5b05719c535c66f4c5f92b8fc0b7cfe5f08cc389
aslanbek <men123123men@yandex.ru>
2018-09-24 14:35:13 +0300

    workflow yaml file format

--- src/main/scala/ru/sber/cb/ap/gusli/actor/projects/writeres/MetaToYamlSerialization.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/projects/writeres/MetaToYamlSerialization.scala
@@ -1,6 +1,7 @@
 package ru.sber.cb.ap.gusli.actor.projects.writeres
 
-import ru.sber.cb.ap.gusli.actor.core.{CategoryMeta, EntityMeta, WorkflowMeta}
+import ru.sber.cb.ap.gusli.actor.core.dto.WorkflowDto
+import ru.sber.cb.ap.gusli.actor.core.{CategoryMeta, EntityMeta}
 
 object MetaToYamlSerialization {
   //  trait CategoryMeta {
@@ -16,78 +17,73 @@ object MetaToYamlSerialization {
   //  }
 
 
-  //  trait WorkflowMeta {
-  //    def grenkiVersion: Option[String]
-  //    def queue: Option[String]
-  //    def user: Option[String]
-  //    def init: Map[String, String]
-  //    def sql: Map[String, String]
-  //    def sqlMap: Map[String, String]
+//  case class WorkflowDto(name:     String,                                название файла или папки
 
-  //    def name: String  // название файла или папки
+//                         grenkiVersion: Option[String] = None,
+//                         queue:    Option[String] = None,
+//                         user:     Option[String] = None,
 
+//                         sql:      Map[String, String],                   .sql    файлы вокруг
+//                         sqlMap:   Map[String, String] = Map.empty,       .config файлы вокруг
+//                         init:     Map[String, String] = Map.empty,       .hql    файлы вокруг  (с большой вероятностью .sql)
 
-
-
-
-  //    def params: Map[String, String]
-  //    def stats: Set[Long]
-  //  }
+//                         params:   Map[String, String] = Map.empty,
+//                         stats:    Set[Long] = Set.empty,
+//                         entities: Set[Long] = Set.empty)
 
 //  meta.yaml
 //  ---
 //  grenki: 0.2
 //  queue: root.platform
 //  user: pupkin
+
 //  init:
-//    - init.hql
+//    - -init.hql
 //    - init2.hql
 //  map:
 //    - map.config
 //    - map2.config
-//
 //  sql:
 //    - rb-vek5555sel.sql
 //    - rb-car433ds.sql
+
 //  param:
-//    - p1: 1
+//    - p1:
 //    - p2: I'm String
 //  stats:
 //    - 2
 //    - 11
 //  entities:
 //    - 123
-//    - 124
+//    - -124
 
 
 
 
 
-  //trait EntityMeta {
-  //  def id: Long
-  //  def name: String
-  //  def path: String
-  //  def parentId: Option[Long]
-  //  def storage = "HDFS"
-  //}
+
   def convertCategoryMetaToYAMLFileContent(meta: CategoryMeta, parentMeta: CategoryMeta): String = {
 
 
     "CategoryMeta  YAMLfile  Content"
   }
 
-  def convertWorkflowMetaToYAMLFileContent(meta: WorkflowMeta, categoryMeta: CategoryMeta): String = {
+  def convertWorkflowMetaToYAMLFileContent(meta: WorkflowDto, categoryMeta: CategoryMeta): String = {
 
 
     "WorkflowMeta  YAMLfile  Content"
   }
 
+
+  //trait EntityMeta {
+  //  def id: Long          в названии файла(или папки)
+  //  def name: String      в названии файла(или папки)
+  //  def path: String
+  //  def parentId: Option[Long]  получаем из родительской папки
+  //  def storage = "HDFS"        всегда "HDFS"
+  //}
   def convertEntityMetaToYAMLFileContent(meta: EntityMeta, parentMeta: EntityMeta): String = {
     s"""---
-       |name: "${meta.name}"
-       |path: "${meta.path}"
-       |id: ${meta.id}
-       |storage: "${meta.storage}"
-       |parentId: ${meta.parentId}""".stripMargin
+       |path: "${meta.path}"""".stripMargin
   }
 }
