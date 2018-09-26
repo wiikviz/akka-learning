2402fe51271adf464a8a1ab3411e85b2dec21c6f
anastasi-snow-maiden <anastasia.aan@yandex.ru>
2018-09-26 15:36:58 +0300

    Corrections of English phrases

--- src/test/scala/ru/sber/cb/ap/gusli/actor/core/entity/EntitySpec.scala
+++ src/test/scala/ru/sber/cb/ap/gusli/actor/core/entity/EntitySpec.scala
@@ -9,22 +9,22 @@ class EntitySpec extends ActorBaseTest("EntitySpec") {
   "An Entity" when {
     val meta = EntityMetaDefault(0, "root-entity", "file.ent", None)
     val entity: ActorRef = system.actorOf(Entity(meta))
-    "receive GetEntityMeta" should {
-      "send back EntityMetaResponse" in {
+    "GetEntityMeta is received" should {
+      "EntityMetaResponse should be sent back" in {
         entity ! GetEntityMeta()
         expectMsg(EntityMetaResponse(meta))
       }
     }
 
-    "receive GetChildren" should {
-      "send back an empty ChildrenEntityList" in {
+    "GetChildren is received" should {
+      "an empty ChildrenEntityList should be sent back" in {
         entity ! GetChildren()
         expectMsg(ChildrenEntityList(Nil))
       }
     }
 
-    "receive AddChildEntity" should {
-      "send back EntityCreated" in {
+    "AddChildEntity  is received" should {
+      "EntityCreated should be sent back" in {
         val meta = EntityMetaDefault(1, "child-entity", "file.ent", None)
         entity ! AddChildEntity(meta)
         expectMsgPF() {
@@ -35,8 +35,8 @@ class EntitySpec extends ActorBaseTest("EntitySpec") {
       }
     }
 
-    "receive again GetChildren" should {
-      "send back list with size 1" in {
+    "GetChildren is received again" should {
+      "a list of size 1 should be sent back" in {
         entity ! GetChildren()
         expectMsgPF() {
           case ChildrenEntityList(list) => assert(list.size == 1)
