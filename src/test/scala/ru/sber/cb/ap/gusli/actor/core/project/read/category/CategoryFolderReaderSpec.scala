318f5604c160248b4e3681a28621b3a26d4b7391
IBagurtsev <shandro94@mail.ru>
2018-09-26 16:22:54 +0300

    #2 add tests for CategoryReader

--- src/test/scala/ru/sber/cb/ap/gusli/actor/core/project/read/category/CategoryFolderReaderSpec.scala
+++ src/test/scala/ru/sber/cb/ap/gusli/actor/core/project/read/category/CategoryFolderReaderSpec.scala
@@ -18,6 +18,9 @@ class CategoryFolderReaderSpec extends ActorBaseTest("DirectoryProjectSpec") {
   type Category = ActorRef
   "CategoryFolderReader" when {
     var cbCategory: Category = null
+    var apCategory: Category = null
+    var rbCategory: Category = null
+    
     "receive ReadCategoryFolder" should {
       "read categories" in {
         categoryFolderReader ! ReadCategoryFolder()
@@ -34,20 +37,34 @@ class CategoryFolderReaderSpec extends ActorBaseTest("DirectoryProjectSpec") {
         }
       }
     }
-    "cb category receiving ListSubcategories" should {
-      "send back list with size 1" in {
-        cbCategory ! ListSubcategory()
-        expectMsgPF() {
-          case SubcategoryList(actorList) => assert(actorList.size == 1)
-        }
-      }
-    }
     "cb category receiving ListWorkflow" should {
       "send back list with size 5" in {
         cbCategory ! ListWorkflow()
         expectMsgPF() {
           case WorkflowList(actorList) => assert(actorList.size == 5)
         }
+      }
+    }
+    "receiving ListSubcategory send back SubcategoryList with size 1 (ap)" in {
+      cbCategory ! ListSubcategory()
+      expectMsgPF() {
+        case SubcategoryList(list) =>
+          assert(list.size == 1)
+          apCategory = list(0)
+      }
+    }
+    "ap category should include rb category" in {
+      apCategory ! ListSubcategory()
+      expectMsgPF() {
+        case SubcategoryList(list) =>
+          assert(list.size == 1)
+          rbCategory = list(0)
+      }
+    }
+    "rb category receiving ListWorkflow send back WorkflowList with size 3" in {
+      rbCategory ! ListWorkflow()
+      expectMsgPF() {
+        case WorkflowList(list) => assert(list.size == 3)
       }
     }
   }
