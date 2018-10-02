807ac10d7437fc3d5a7fa84c4513627390178712
IBagurtsev <shandro94@mail.ru>
2018-10-02 18:34:04 +0300

    test difff

--- src/test/scala/ru/sber/cb/ap/gusli/actor/core/diff/category/recursive/RecursiveCategoryDiffForNonEqualsSpec.scala
+++ src/test/scala/ru/sber/cb/ap/gusli/actor/core/diff/category/recursive/RecursiveCategoryDiffForNonEqualsSpec.scala
@@ -13,13 +13,13 @@ import scala.concurrent.duration._
 import scala.concurrent.duration._
 
 class RecursiveCategoryDiffForNonEqualsSpec extends ActorBaseTest("RecursiveCategoryDiffForNonEqualsSpec") {
-  private val correctPath = Paths.get("./src/gucli/src/test/scala/ru/sber/cb/ap/gusli/actor/core/diff/category/recursive/data/project")
+  private val correctPath = Paths.get("./src/test/resources/project_k")
   private val directoryProjectReader: ActorRef = system.actorOf(DirectoryProjectReader(correctPath))
 
   directoryProjectReader ! ReadProject()
 
-  "Directory project writer" when {
-    expectMsgPF(1 minute) {
+  "Directory project differ" when {
+    expectMsgPF() {
       case ProjectReaded(project) =>
     }
   }
