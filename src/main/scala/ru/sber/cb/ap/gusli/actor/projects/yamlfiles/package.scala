146df8fa999308f1d80fb8e734450679b74b5ad4
IBagurtsev <shandro94@mail.ru>
2018-10-05 13:36:36 +0300

    #1 do not write yaml doc start marker

--- src/main/scala/ru/sber/cb/ap/gusli/actor/projects/yamlfiles/package.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/projects/yamlfiles/package.scala
@@ -6,11 +6,10 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 package object yamlfiles {
-  def initMapper: ObjectMapper = {
-    val mapper: ObjectMapper = new ObjectMapper(new YAMLFactory().enable(Feature.MINIMIZE_QUOTES))
+  def initMapper: ObjectMapper =  new ObjectMapper(new YAMLFactory()
+    .enable(Feature.MINIMIZE_QUOTES)
+    .disable(Feature.WRITE_DOC_START_MARKER))
       .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
       .enable(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED)
       .registerModule(DefaultScalaModule)
-    mapper
-  }
 }
