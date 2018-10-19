38bc478e622ccd2c22c5b6b18260fa4429c7664c
Kviz <kviz@outlook.com>
2018-10-19 15:47:00 +0300

    format code

--- src/main/scala/ru/sber/cb/ap/gusli/actor/core/dto/ProjectDto.scala
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/core/dto/ProjectDto.scala
@@ -2,4 +2,4 @@ package ru.sber.cb.ap.gusli.actor.core.dto
 
 import ru.sber.cb.ap.gusli.actor.core.ProjectMeta
 
-case class ProjectDto(name:String, entityRoot:EntityDto, categoryRoot:CategoryDto) extends ProjectMeta
+case class ProjectDto(name: String, entityRoot: EntityDto, categoryRoot: CategoryDto) extends ProjectMeta
